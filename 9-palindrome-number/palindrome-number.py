4d85eb812be20f8d0efea5a1b605a19dda5d56de
FareedKhan Pathan <freedkhan081106@gmail.com>
2026-08-04 19:31:59 +0530

    Time: 8 ms (53.43%) | Memory: 19.3 MB (18.63%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,12 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+        if x<0:
             return False
-
-        n = 0
-        arr = list(map(int, str(x)))
-        for x in range(len(arr)-1, 0, -1):
-            if arr[x] != arr[n]:
-                return False
-            n += 1
-        return True
+        s = str(x)
+        return s==s[::-1]
